d4cf13119d144235178fbe8d3f304619db411e76
NandhipatiGanesh <126049941+NandhipatiGanesh@users.noreply.github.com>
2025-03-11 00:19:52 +0530

    first commit

---
+++ src/app/components/footer/footer.tsx
@@ -0,0 +1,93 @@
+import React, { memo } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import './footerstyles.css';
+const data = {
+  description: 'Lorem ipsum dolor sit amet consectetur. Aliquet morbi amet nec diam. Vel massa commodo viverra.Lorem ipsum dolor sit amet consectetur. Aliquet morbi amet nec diam',
+  logo: '/lux logo final .png',
+  forpatients: [
+    { text: 'About Us', url: '#' },
+    { text: 'FAQs', url: '#' },
+    { text: 'Contact Us', url: '#' },
+    { text: 'Blogs', url: '#' }
+  ],
+  centerofexcellence: [
+    { text: 'Cardiology', url: '#' },
+    { text: 'Orthopedics', url: '#' },
+    { text: 'Gastroenterology', url: '#' },
+    { text: 'Oncology', url: '#' }
+  ],
+  followus: [
+    { text: 'Facebook', url: '#', img: '/youtube logo.png' },
+    { text: 'Instagram', url: '#', img: '/youtube logo.png' },
+    { text: 'LinkedIn', url: '#', img: '/youtube logo.png' },
+    { text: 'YouTube', url: '#', img: '/youtube logo.png' }
+  ]
+};
+
+const FooterComponent = () => {
+  return (
+    <section className="footer bg-white w-full py-8">
+      <div className="inner_footer">      
+        <div className="column c_one">
+          <Link href="#" className='footer_logo' style={{
+            marginBottom: '20px'
+          }}>
+            <Image src={data.logo} alt="Logo" width={200} height={60} className=''/>
+          </Link>
+          <p className="text-gray-600 mt-2 footer_description">{data.description}</p>
+        </div>
+      <div  className='inner_second_footer'> 
+      <div className="column c_two ">
+          <h3 className="font-semibold mb-2">For Patients</h3>
+          <ul>
+            {data.forpatients.map((item, index) => (
+              <li key={index} className='for_patients'>
+                <Link href={item.url}>
+                  <span className="text-gray-700 hover:text-blue-500 cursor-pointer">
+                    {item.text}
+                  </span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="column c_three w-full sm:w-1/4">
+          <h3 className="font-semibold mb-2">Center of Excellence</h3>
+          <ul>
+            {data.centerofexcellence.map((item, index) => (
+              <li key={index} className='excellence_list'>
+                <Link href={item.url}>
+                  <span className="text-gray-700 hover:text-blue-500 cursor-pointer">
+                    {item.text}
+                  </span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="column c_four w-full sm:w-1/4">
+          <h3 className="font-semibold mb-2">Follow Us</h3>
+          <div className='column c_social_footer'>
+            {data.followus.map((item, index) => (
+              <div key={index} className='socials_div flex' >
+                <Link href={item.url}>
+                  <Image src={item.img} alt={item.text} width={24} height={24} />
+                </Link>
+                <p>{item.text}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      
+      </div>
+    </section>
+  );
+};
+
+export default FooterComponent;
+
---
+++ src/app/components/header/header.jsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import "./headerstyles.css";
+
+export default function Header() {
+    const logos = [
+        "/lux logo final .png",
+        "/nabh.png",
+        "/trail.png",
+    ];
+
+    const menuItems = [
+        { id: 1, name: "Home", url: "/" },
+        { id: 2, name: "About", url: "/about" },
+        { id: 3, name: "Services", url: "/services" },
+        { id: 4, name: "Contact", url: "/contact" },
+    ];
+
+    const cta = { name: "+91123456789", url: "/get-started" };
+
+    return (
+        <section className="header">
+            <div class="header__logos">
+    <a href="/"><img className="header__logo" src="/lux logo final .png" /></a>
+    <a href="/"><img className="header__logotwo" src="/nabh.png" /></a>
+    <a href="/"><img className="header__logothree" src="/trail.png" /></a>
+</div>
+
+
+            <nav className="header__menu">
+                <ul>
+                    {menuItems.map((item) => (
+                        <li key={item.id}>
+                            <Link href={item.url}>{item.name}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+
+            <div className="header__cta">
+                <Link href={cta.url} className="header_cta_type_one">{cta.name}</Link>
+            </div>
+        </section>
+    );
+}
