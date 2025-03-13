fba37c6c3fb8ca3a6c583e41e3c799ad5581fc0c
NandhipatiGanesh <162296386+ganeshadvait@users.noreply.github.com>
2025-03-13 15:50:27 +0530

    faqs added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 
 import Hero from './components/hero/hero';
+import Faqs from './components/faqs/faq';
 export default function Home() {
   return (
     <>
     <Hero />
+    <Faqs />
     </>
   );
 }
