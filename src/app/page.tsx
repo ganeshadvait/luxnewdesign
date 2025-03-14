5f51047a816d10a8b2c23576dc6017631a7e87a2
NandhipatiGanesh <162296386+ganeshadvait@users.noreply.github.com>
2025-03-14 18:51:12 +0530

    videos added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,14 @@ import Hero from './components/hero/hero';
 import Faqs from './components/faqs/faq';
 import Whatare from './components/what/whatare';
 import Luxgpt from './components/luxgpt/luxgpt';
+import VideoSection from './components/Videosection/videos';
+import PatientTalks from './components/Patienttalks/patients';
 export default function Home() {
   return (
     <>
     <Hero />
+    <VideoSection />
+    <PatientTalks />
     <Luxgpt />
     <Whatare />
     <Faqs />
