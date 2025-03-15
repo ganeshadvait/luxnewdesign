02d2205b56935bb3040ef2f45447c40565c4f637
NandhipatiGanesh <162296386+ganeshadvait@users.noreply.github.com>
2025-03-15 18:32:00 +0530

    state added

--- src/app/components/second/second.jsx
+++ src/app/components/second/second.jsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from 'next/image';
-import React from 'react';
+import React, { useState} from 'react';
 import './sstyles.css';
 import { useInView } from 'react-intersection-observer';
 
