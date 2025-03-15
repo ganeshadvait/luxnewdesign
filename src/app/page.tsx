e74c499e5f6a7144227fbae14aa406466b99f9c2
NandhipatiGanesh <162296386+ganeshadvait@users.noreply.github.com>
2025-03-15 19:31:20 +0530

    3 hide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,17 @@ import Luxgpt from './components/luxgpt/luxgpt';
 import VideoSection from './components/Videosection/videos';
 import PatientTalks from './components/Patienttalks/patients';
 import Second from './components/second/second';
-import HospitalLocation from './components/HospitalLocation/hospitallocations';
-import HospitalComparison from './components/Treatmenttable/Hospitalcomparison';
-import Accreditations from './components/Accreditions/accreditions';
+// import HospitalLocation from './components/HospitalLocation/hospitallocations';
+// import HospitalComparison from './components/Treatmenttable/Hospitalcomparison';
+// import Accreditations from './components/Accreditions/accreditions';
 export default function Home() {
   return (
     <>
     <Hero />
     <Second />
-    <HospitalLocation />
+    {/* <HospitalLocation />
     <HospitalComparison />
-    <Accreditations />
+    <Accreditations /> */}
     <VideoSection />
     <PatientTalks />
     <Luxgpt />
